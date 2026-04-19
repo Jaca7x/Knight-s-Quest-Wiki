1d9859e76c2e144c9dd8f5fb153d3a70d6da2e8c
Jaca <eduardo.costa.miguel@escola.pr.gov.br>
2026-04-19 10:46:59 -0300

    feat: seção de sistemas completa

--- src/features/technologies/components/GameSystems.tsx
+++ src/features/technologies/components/GameSystems.tsx
@@ -1,26 +1,40 @@
-import { gameSystems } from "@/data/gameSystemsData";
+import { gameSystems, type GameSystemsData } from "@/data/gameSystemsData";
+import GameSystemsModal from "@/features/technologies/components/GameSystemsModal"
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function GameSystems() {
+    const [selectSystem, setSelectSystem] = useState<GameSystemsData | null>(null);
+
+    useEffect(() => {
+        if (selectSystem) {
+            document.documentElement.style.overflow = 'hidden';
+        } else {
+            document.documentElement.style.overflow = 'unset';
+        }
+        return () => { document.documentElement.style.overflow = 'unset'; };
+    }, [selectSystem]);
+
     return (
         <section className="py-20 px-4 bg-[#120f1d] flex justify-center">
-            {/* GRID: Mantive o 2 colunas no MD e 1 no mobile */}
             <div className="grid md:grid-cols-2 grid-cols-1 max-w-5xl w-full gap-10">
 
                 {gameSystems.map((system) => (
                     <div
+                        role="button"
+                        tabIndex={0}
                         key={system.id}
-                        className="group flex flex-col h-full bg-[#0d0b16] border-2 border-[#3B354F] rounded-2xl overflow-hidden hover:border-[#FACC15]/60 transition-all duration-500 hover:-translate-y-1 shadow-2xl"
+                        onClick={() => setSelectSystem(system)}
+                        className="group flex flex-col h-full bg-[#0d0b16] border-2 border-[#3B354F] rounded-2xl overflow-hidden hover:border-[#FACC15]/60 transition-all duration-500 hover:-translate-y-1 shadow-2xl cursor-pointer focus-visible:ring-2 focus-visible:ring-[#FACC15] focus-visible:border-[#FACC15] focus-visible:ring-offset-8 focus-visible:ring-offset-[#FACC15]"
                     >
-                        {/* HEADER DO CARD */}
                         <div className="bg-[#161326] py-4 border-b-2 border-[#3B354F] group-hover:border-[#FACC15]/60 transition-colors duration-500 flex items-center justify-center">
                             <h1 className="uppercase text-[#FACC15] font-mono font-bold md:text-lg text-base tracking-[0.2em] px-4 text-center">
                                 {system.title}
                             </h1>
                         </div>
 
-                        {/* ÁREA DA IMAGEM / GIF: Altura fixa para manter o alinhamento do grid */}
-                        <div className="flex-1 flex items-center justify-center bg-black/20 p-6 min-h-62.5 md:min-h-75">
-                            <div className="relative w-full h-full flex items-center justify-center">
+                        <div className="flex-1 flex items-center justify-center bg-black/20 p-6 min-h-62.5 md:min-h-75 " >
+                            <div className="relative w-full h-full flex items-center justify-center ">
                                 <img
                                     src={system.gif}
                                     alt={system.title}
@@ -29,7 +43,6 @@ export default function GameSystems() {
                             </div>
                         </div>
 
-                        {/* DICA DE CLIQUE (RODAPÉ) */}
                         <div className="py-3 text-center bg-[#161326] border-t-2 border-[#3B354F] group-hover:border-[#FACC15]/60 transition-all duration-500">
                             <span className="text-xs md:text-sm font-mono text-[#FACC15] tracking-[0.15em] font-bold">
                                 EXPLORAR IMPLEMENTAÇÃO EM C
@@ -38,6 +51,11 @@ export default function GameSystems() {
                     </div>
                 ))}
             </div>
+
+            <AnimatePresence>
+                {selectSystem && (<GameSystemsModal data={selectSystem} onClose={() => setSelectSystem(null)} />
+                )}
+            </AnimatePresence>
         </section>
     );
 }
